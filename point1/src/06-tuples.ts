0de723dff4d6aaf15fdc68a8ace01727b38bb92d
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-25 13:58:44 +0200

    done 05-arrays and 06-tuples

---
+++ point1/src/06-tuples.ts
@@ -0,0 +1,10 @@
+const pairs: [string, string][] = [
+  ['key', 'value'],
+  ['key2', 'value3'],
+];
+const data: [number, boolean, string] = [1, true, 'asdasd'];
+
+//csv
+
+const doc: [string, string, number, Date][] = [];
+doc.push(['Bohdan', 'Kut', 12, new Date()]);
