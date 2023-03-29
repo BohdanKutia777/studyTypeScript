216cfcf476d9892acf5273f3d3c3e6cc5fe15965
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-29 14:45:23 +0300

    done 11-types-merge

---
+++ point1/src/11-types-merge.ts
@@ -0,0 +1,40 @@
+type Union1 = 'a' | 'b' | 'c' | 'd';
+type Union2 = 'a' | 'e' | 'c' | 'i';
+
+type Union3 = Union1 | Union2; // all variant
+
+type Union4 = Union1 & Union2; // a | c
+type Union6 = { a: string; b: string; c: number } & {
+  a: string;
+  t: boolean;
+  z: null;
+};
+// const example: Union6 = {
+
+// }
+type User1 = {
+  readonly email: string;
+  readonly login: string;
+  password: string;
+};
+
+type Person1 = {
+  readonly firstName: string;
+  lastName: string;
+  phone?: string;
+  yearsOfBirth?: number;
+};
+
+type Employee1 = {
+  contractStart: Date;
+} & User1 &
+  Person1;
+const user1: Employee1 = {
+  contractStart: new Date(),
+};
+type Dev1 = {
+  skills: string[];
+  level?: 'junior' | 'middle' | 'senior';
+  say(): void;
+  code?: (arg: string) => void;
+} & Employee1;
