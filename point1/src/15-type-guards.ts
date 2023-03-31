01c776eb1742dbd64cc16900926bac4c9e3a4595
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-31 20:40:39 +0300

    done 15-type-guards

---
+++ point1/src/15-type-guards.ts
@@ -0,0 +1,17 @@
+type Fish1 = { swim: () => void };
+type Bird1 = { fly: () => void };
+
+function isFish(pet: Fish1 | Bird1): pet is Fish1 {
+  return (pet as Fish1).swim !== undefined;
+}
+
+function move(animal: Fish | Bird) {
+  if (isFish(animal)) {
+    return animal.swim();
+  }
+  return animal.fly();
+}
+
+function isNull(val: any): val is null {
+  return !val;
+}
