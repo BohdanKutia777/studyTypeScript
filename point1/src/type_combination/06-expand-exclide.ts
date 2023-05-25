c197ea6c8e1e9e013d475c373a177df5e7d8f9bf
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-05-25 21:48:35 +0300

    done task 06-expand-exclide

---
+++ point1/src/type_combination/06-expand-exclide.ts
@@ -0,0 +1,28 @@
+export {};
+
+type T0 = Exclude<'a' | 'b' | 'c', 'a'>;
+type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>;
+type T2 = Exclude<string | number | (() => void), Function>;
+
+type Status = 'success' | 'clientError' | 'serverError' | 200 | 401 | 504;
+type NumericStatus = Exclude<Status, string>;
+type TextStatus = Exclude<Status, number>;
+
+type T00 = Extract<'a' | 'b' | 'c', 'a'>;
+type T10 = Extract<'a' | 'b' | 'c', 'a' | 'b' | 'd'>;
+type T20 = Extract<string | number | (() => void), Function>;
+
+interface Person {
+  age: number;
+  firstName: string;
+  lastName: string;
+  sex: 'male' | 'female';
+  country: string;
+  education: string;
+  skills: string[];
+}
+
+type PersonNames = Extract<keyof Person, 'firstName' | 'lastName' | 'fullName'>;
+
+type T30 = NonNullable<string | number | undefined>;
+type T31 = NonNullable<string[] | null | undefined>;
